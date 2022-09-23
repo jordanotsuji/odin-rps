2bc940f398b90e421f9984579130c98e39fb5b01
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-09-23 11:58:11 -1000

    Completed single round rps function and player input function

--- rps.js
+++ rps.js
@@ -1,1 +1,49 @@
-console.log("hi");
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playerSelection() {
+  let choice = prompt("Rock, Paper, Scissors, Shoot: ");
+  while (!choices.includes(choice.toLowerCase())) {
+    console.log("Please choose a valid option")
+    choice = prompt("Rock, Paper, Scissors, Shoot: ");
+  }
+  return choice.toLowerCase();
+}
+
+function playRound(player, computer) {
+  player = player.toLowerCase();
+  computer = computer.toLowerCase();
+
+  if (player == computer) {
+    return "draw!"
+  }
+  else if (player == "rock") {
+    if (computer == "paper") {
+      return "You Lose!"
+    } else if (computer == "scissors") {
+      return "You Win!"
+    }
+  }
+  else if (player == "paper") {
+    if (computer == "scissors") {
+      return "You Lose!"
+    } else if (computer == "rock") {
+      return "You Win!"
+    }
+  }
+  else {
+    if (computer == "rock") {
+      return "You Lose!"
+    } else if (computer == "paper") {
+      return "You Win!"
+    }
+  }
+}
+
+// Gameplay Area
+
+console.log("Welcome to Rock Paper Scissors!");
+
