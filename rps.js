be1aa292fc5b8d906e02febd65fc0f36b01ce554
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-10-05 11:22:34 -1000

    formatting

--- rps.js
+++ rps.js
@@ -1,22 +1,12 @@
 // Rock Paper Scissors code
-// Functions: getComputerChoice()
-//            playerSelectiOn()
-//            playRound(player choice, computer choice)
 
+// Gameplay Variables
 let playerScore = 0;
 let computerScore = 0;
 let inRound = false;
 const choices = ["rock", "paper", "scissors"];
 
-// DOM elements
-const playerChoiceBoxes = document.querySelectorAll('.player .choice-box')
-const winsText = document.querySelector('.wins')
-const lossesText = document.querySelector('.losses')
 
-
-function getComputerChoice() {
-  return choices[Math.floor(Math.random() * choices.length)];
-}
 
 function playerSelection(e) {
   if (inRound) return;   // If in the middle 
@@ -35,41 +25,11 @@ function playerSelection(e) {
   inRound = false;
 }
 
-function playRound(player, computer) {
-  player = player.toLowerCase();
-  computer = computer.toLowerCase();
-
-  if (player == computer) {
-    return "draw"
-  }
-  else if (player == "rock") {
-    if (computer == "paper") {
-      return "lose"
-    } else if (computer == "scissors") {
-      return "win"
-    }
-  }
-  else if (player == "paper") {
-    if (computer == "scissors") {
-      return "lose"
-    } else if (computer == "rock") {
-      return "win"
-    }
-  }
-  else {
-    if (computer == "rock") {
-      return "lose"
-    } else if (computer == "paper") {
-      return "win"
-    }
-  }
-}
-
 function startRound(playerChoice) {
+
   // slot machine animation
 
   let result = playRound(playerChoice, getComputerChoice())
-
   if (result === "win") {
     winsText.textContent = `Wins: ${++playerScore}`
   } else if (result === "lose") {
@@ -77,17 +37,45 @@ function startRound(playerChoice) {
   }
 }
 
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
 
+
+function playRound(player, computer) {
+  player = player.toLowerCase();
+  computer = computer.toLowerCase();
+
+  if (player == computer)
+    return "draw"
+  else if (player == "rock") {
+    if (computer == "paper")
+      return "lose"
+    else if (computer == "scissors")
+      return "win"
+  }
+  else if (player == "paper") {
+    if (computer == "scissors")
+      return "lose"
+    else if (computer == "rock")
+      return "win"
+  }
+  else if (player == "scissors")
+    if (computer == "rock")
+      return "lose"
+    else if (computer == "paper")
+      return "win"
+}
+
+
+//
 // Animations Area
+//
+
+// DOM elements
+const playerChoiceBoxes = document.querySelectorAll('.player .choice-box')
+const winsText = document.querySelector('.wins')
+const lossesText = document.querySelector('.losses')
+
 playerChoiceBoxes.forEach(playerChoiceBox => playerChoiceBox.addEventListener('click', playerSelection))
 
-// Gameplay Area
-
-// console.log("Welcome to Rock Paper Scissors!");
-// let games = prompt("How many games would you like to play?")
-
-
-// for (let i = 0; i < games; i++) {
-//   let playerChoice = playerSelection();
-//   console.log(playRound(playerChoice, getComputerChoice()));
-// }
